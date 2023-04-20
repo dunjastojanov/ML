393a8765f8e476c5a4d085df56b12c28679e5a32
MilosMilovanovic00 <102919389+MilosMilovanovic00@users.noreply.github.com>
2023-04-20 11:39:52 +0200

    Added emoji replacement Added removing stop words

--- z03/svm.py
+++ z03/svm.py
@@ -13,46 +13,74 @@ def transform_review_sentence(review):
     return review.lower()
 
 
-def replace_emojis(review):
-    if ':)' in review:
-        review = review.replace(":)", "dobro")
-    if ';)' in review:
-        review = review.replace(";)", "super")
-    if 'xD' in review:
-        review = review.replace("xD", "fantasticno")
-    if ':-)' in review:
-        review = review.replace(":-)", "dobro")
-    if ';D' in review:
-        review = review.replace(";D", "bez veze")
-    if ':(' in review:
-        review = review.replace(":(", "lose")
-    return review
+STOPWORDS = ['koja', 'tim', 'onakve', 'tome', 'onolikim', 'si', 'ovolikom', 'onakvim', 'nekog', 'gde', 'i', 'takvim',
+             'onoliki', 'onakav', 'ovoliku', 'onakvu', 'ovakvim', 'kojeg', 'ovakvo', 'onolikom', 'neke', 'o',
+             'onolikoj', 'bio', 'kuda', 'ovolika', 'na', 'one', 'onakvoj', 'tolikim', 'ka', 'bilo', 'kakve', 'skoro',
+             'odakle', 'onakvom', 'za', 'koliki', 'kolike', 'da', 'ako', 'kojom', 'te', 'tolike', 'onoliku', 'jednom',
+             'odmah', 'tolikom', 'neka', 'takvu', 'ali', 'tolikog', 'onoliko', 'kojim', 'tom', 'koji', 'smo', 'ovu',
+             'takav', 'kakvom', 'čim', 'šta', 'sa', 'što', 'ovoliko', 'jednog', 'onoj', 'ko', 'kako', 'spreman',
+             'bila', 'još', 'ovo', 'takvoj', 'jesam', 'koga', 'u', 'to', 'ovakav', 'biti', 'zašto', 'ste', 'sam',
+             'toliki', 'kod', 'onim', 'ona', 'jednoj', 'ono', 'ovakvog', 'chim', 'tolika', 'ovom', 'ovim', 'kad',
+             'takvog', 'kakav', 'do', 'jednim', 'bas', 'sta', 'ja', 'takodje', 'jedne', 'ovaj', 'od', 'jedno',
+             'ovoliki', 'onog', 'kakva', 'kakvim', 'štaviše', 'takvo', 'upravo', 'koliko', 'bezmalo', 'jedna',
+             'onaj', 'ovakve', 'onolikog', 'pri', 'onakvo', 'sada', 'ovakvoj', 'ovolikim', 'onakva', 'kojoj', 'ove',
+             'onolike', 'baš', 'koju', 'ovolikog', 'ovolike', 'neki', 'gotovo', 'oko', 'kakvoj', 'takve', 'onolika',
+             'ovakvom', 'nekim', 'tu', 'odmakh', 'sto', 'nekom', 'pored', 'tolikoj', 'takode', 'zbog', 'kakvo', 'ova',
+             'kakvog', 'jedan', 'taj', 'jesi', 'kojem', 'toliko', 'ovog', 'jos', 'toj', 'ili', 'jesmo', 'je',
+             'ovolikoj', 'kao', 'a', 'koje', 'onakvog', 'jesu', 'onom', 'su', 'tog', 'jeste', 'ovakva', 'zasto',
+             'ovoj', 'jednu', 'kakvu', 'kolika', 'jer', 'takvom', 'toliku', 'iz', 'nekoj', 'ta', 'otkuda', 'onu',
+             'kada', 'takva', 'neko', 'se', 'ja', 'ti', 'on', 'ona', 'ono', 'mi', 'vi', 'oni', 'one', 'vam', 'ga', 'im',
+             'ko', 'šta', "što", "nešto", "svako", "ih", "al", "bi"]
+
+EMOTICONS = {
+    ":)": "smeh",
+    ":(": "tuga",
+    ";)": "namig",
+    ":D": "smeh",
+    ":-)": "smeh",
+    ":-(": "tuga",
+    ";-)": "namig",
+    ":'(": "tuga",
+    ":')": "radost",
+    ":|(": "iznenađenje",
+    ":|)": "iznenađenje",
+    ":o)": "šok",
+    ":o(": "šok",
+    ":p": "jezik",
+    ":P": "jezik",
+    ":d": "jezik",
+    ":))": "smeh",
+    ":)))": "smeh",
+    ":((": "tuga",
+    ":(((": "tuga",
+}
 
 
 def remove_stop_words(review):
-    # TODO implement removing stop words
-    pass
+    return " ".join([word for word in review.split() if word not in STOPWORDS])
 
 
-def remove_serbian_specific_letters(review):
-    # TODO implement replacing letters like ŠĐĆČŽ with s,dj,c,c,z
-    pass
+def remove_punctuation(review):
+    return "".join([char for char in review if char not in string.punctuation])
+
+
+def replace_emoticons(review):
+    return " ".join([EMOTICONS[word] if word in EMOTICONS.keys() else word for word in review.split()])
+
+
+def transform_dataset(dataset):
+    dataset['Review'] = dataset['Review'].apply(transform_review_sentence)
+    dataset['Review'] = dataset['Review'].apply(replace_emoticons)
+    dataset['Review'] = dataset['Review'].apply(remove_punctuation)
+    dataset['Review'] = dataset['Review'].apply(remove_stop_words)
+    dataset['Sentiment'] = dataset['Sentiment'].apply(transform_zeros)
+    return dataset
 
 
 def transform_zeros(sentiment):
     if sentiment == 0:
         return -1
     return sentiment
-
-
-def transform_dataset(dataset):
-    dataset['Review'] = dataset['Review'].apply(replace_emojis)
-    dataset['Review'] = dataset['Review'].apply(transform_review_sentence)
-    # TODO uncomment when done
-    # dataset['Review'] = dataset['Review'].apply(remove_serbian_specific_letters)
-    # dataset['Review'] = dataset['Review'].apply(remove_stop_words)
-    dataset['Sentiment'] = dataset['Sentiment'].apply(transform_zeros)
-    return dataset
 
 
 def split_dataset_on_x_y(dataset):
@@ -66,7 +94,7 @@ def linear_kernel():
     y_pred = clf.predict(X_test)
     accuracy = sklearn.metrics.f1_score(y_test, y_pred, average='micro')
     # accuracy = accuracy_score(y_test, y_pred)
-    print("Accuracy:", accuracy)
+    print(accuracy)
     return accuracy
 
 
